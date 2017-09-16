1ffa280009602983511cc85d4ebf68127a7f128c
Corban10 <Corban30303@hotmail.com>
2017-09-16 14:40:32 +1200

    Island Spawn Basic

---
+++ Assets/Scripts/LandMover.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandMover : MonoBehaviour
+{
+    public static float landSpeed; //static so that it can be accessed from other scripts
+    void Awake()
+    {
+        landSpeed = -2;
+    }
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().velocity = GetComponent<Rigidbody2D>().transform.up * landSpeed;
+    }
+}
---
+++ Assets/Scripts/LandSpawn.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandSpawn : MonoBehaviour
+{    
+    public GameObject island; //to instantiate island sprite
+    public Transform Spawn1;
+    public Transform Spawn2;
+    void Start()
+    {
+        Instantiate(island, Spawn1.position, Spawn1.rotation);
+        Instantiate(island, Spawn2.position, Spawn2.rotation);
+    }
+}
