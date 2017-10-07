639d0f66cc6097e83cea2e365c6d9a261c9ce563
Corban10 <Corban30303@hotmail.com>
2017-10-08 00:48:29 +1300

    ObstacleSpawn
    Obstacles 1 and 2 now spawn at random times, placeholder sprites

--- Assets/Scripts/LandMover.cs
+++ Assets/Scripts/LandMover.cs
@@ -7,7 +7,7 @@ public class LandMover : MonoBehaviour
     public static float landSpeed; //static so that it can be accessed from other scripts
     void Awake()
     {
-        landSpeed = -2;
+        landSpeed = -2.68f;
     }
     void Start()
     {
--- Assets/Scripts/LandSpawn.cs
+++ Assets/Scripts/LandSpawn.cs
@@ -1,15 +1,48 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LandSpawn : MonoBehaviour
 {    
-    public GameObject island; //to instantiate island sprite
+    public GameObject island;
+    public GameObject battleship;
+
     public Transform Spawn1;
     public Transform Spawn2;
+    
+    System.Random R = new System.Random();
     void Start()
     {
-        Instantiate(island, Spawn1.position, Spawn1.rotation);
-        Instantiate(island, Spawn2.position, Spawn2.rotation);
+        //Instantiate(island, Spawn1.position, Spawn1.rotation);
+        StartCoroutine(Spawner());
+    }
+    IEnumerator Spawner()
+    {
+        float spawnTimer = R.Next(10, 15);
+        switch (R.Next(1, 3))
+        {
+            case 1:
+                Instantiate(island, Spawn1.position, Spawn1.rotation);
+                break;
+            case 2:
+                Instantiate(battleship, Spawn1.position, Spawn1.rotation);
+                break;
+        }
+        yield return new WaitForSeconds(spawnTimer);
+        
+        spawnTimer = R.Next(10, 15);
+        switch (R.Next(1, 3))
+        {
+            case 1:
+                Instantiate(island, Spawn2.position, Spawn2.rotation);
+                break;
+            case 2:
+                Instantiate(battleship, Spawn2.position, Spawn2.rotation);
+                break;
+        }
+        yield return new WaitForSeconds(spawnTimer);
+
+        StartCoroutine(Spawner());
     }
 }
